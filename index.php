082d2ed50979a1c95d8edd7e04bfdfb84a339bb7
Manu1ND <manjunath2000@hotmail.com>
2019-12-23 02:01:25 +0530

    google api returns data in json format removed drop-down menu for search type i.e. "Title", "Author"

--- index.php
+++ index.php
@@ -1,188 +1,153 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-      crossorigin="anonymous"
-    />
-    <script
-      src="https://kit.fontawesome.com/97f3c2998d.js"
-      crossorigin="anonymous"
-    ></script>
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:700&display=swap" rel="stylesheet">
-    
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
-    <link rel="stylesheet" href="style.css" />
-
-   
-
-    <title>Document</title>
-  </head>
-  <body>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+  <script src="https://kit.fontawesome.com/97f3c2998d.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:700&display=swap" rel="stylesheet">
 
 
-    <section>
-      <div class="container-fluid form ">
-        <h1 style=" color:#46b5d1;">ADD BOOK</h1>
-        <form>
-          <div class="form-group row align-items-center justify-content-center ">
-            <label for="" class="col-sm-2 col-form-label">Title</label>
-            <div class="col-md-6">
-              <input type="text" class="form-control" id="" placeholder="Example input">
-            </div>
+  <link rel="stylesheet" href="style.css" />
+
+
+
+  <title>Document</title>
+</head>
+
+<body>
+
+
+  <section>
+    <div class="container-fluid form ">
+      <h1 style=" color:#46b5d1;">ADD BOOK</h1>
+      <form>
+        <div class="form-group row align-items-center justify-content-center ">
+          <label for="" class="col-sm-2 col-form-label">Title</label>
+          <div class="col-md-6">
+            <input type="text" class="form-control" id="" placeholder="Example input">
           </div>
-          <div class="form-group row align-items-center justify-content-center ">
-            <label for="" class="col-sm-2 col-form-label">Author</label>
-            <div class="col-sm-6">
-              <input type="text" class="form-control" id="" placeholder="Example input">
-            </div>
+        </div>
+        <div class="form-group row align-items-center justify-content-center ">
+          <label for="" class="col-sm-2 col-form-label">Author</label>
+          <div class="col-sm-6">
+            <input type="text" class="form-control" id="" placeholder="Example input">
           </div>
-          <div class="form-group row align-items-center justify-content-center ">
-            <label for="" class="col-sm-2 col-form-label">ISBN</label>
-            <div class="col-sm-6">
-              <input type="text" class="form-control" id="" placeholder="Example input">
-            </div>
+        </div>
+        <div class="form-group row align-items-center justify-content-center ">
+          <label for="" class="col-sm-2 col-form-label">ISBN</label>
+          <div class="col-sm-6">
+            <input type="text" class="form-control" id="" placeholder="Example input">
           </div>
-          <div class="form-group row align-items-center justify-content-center ">
-            <label for="" class="col-sm-2 col-form-label" >Description</label>
-            <div class="col-sm-6">
-              <textarea class="form-control" id="" rows="3"></textarea>
-            </div>
+        </div>
+        <div class="form-group row align-items-center justify-content-center ">
+          <label for="" class="col-sm-2 col-form-label">Description</label>
+          <div class="col-sm-6">
+            <textarea class="form-control" id="" rows="3"></textarea>
           </div>
-          <div class="form-group form-row align-items-center justify-content-center ">
-            <label for="" class="col-sm-2 col-form-label">Image</label>
-            <div class="col-sm-6">
-              <input type="file" class="form-control-file" id="">
-            </div>
+        </div>
+        <div class="form-group form-row align-items-center justify-content-center ">
+          <label for="" class="col-sm-2 col-form-label">Image</label>
+          <div class="col-sm-6">
+            <input type="file" class="form-control-file" id="">
           </div>
-          <div class="form-group form-row align-items-center justify-content-center ">
-            
-            <label for="" class="col-sm-2 col-form-label">Quantity</label>
-            <div class="col-sm-6">
-              <input type="number" class="form-control" id="" placeholder="Example input">
-            </div>
+        </div>
+        <div class="form-group form-row align-items-center justify-content-center ">
+
+          <label for="" class="col-sm-2 col-form-label">Quantity</label>
+          <div class="col-sm-6">
+            <input type="number" class="form-control" id="" placeholder="Example input">
           </div>
-          <div class="form-group form-row align-items-center justify-content-center ">
-            <div class="col-sm-1 addbook" >
-              <button type="submit" class="btn btn-info btn-lg">Add Book</button>
-            </div>
+        </div>
+        <div class="form-group form-row align-items-center justify-content-center ">
+          <div class="col-sm-1 addbook">
+            <button type="submit" class="btn btn-info btn-lg">Add Book</button>
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
+    </div>
 
 
-    </section>
-    <section>
-      <div class="container-fluid search" >
-        <form id="search-form" method="post">
-          <div
-            class="form-row align-items-center justify-content-center "
-            style="padding-top: 5rem;"
-          >
-            <div class="col-auto">
-             
-              <div class="btn-group">
-                <button type="button " class="form-control btn btn-default btn-lg dropdown-toggle mb-2" data-toggle="dropdown">Search Type<span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu " role="menu">
-                    <li>Title</li>
-                    <li>Author</li>
-                    <li>ISBN</li>
-                    <li>Genre</li>
-                </ul>
-            </div>
-              
-            </div>
-            <div class="col-md-4" >
-              <label class="sr-only " for="inlineFormInput">Name</label>
-              <input
-                type="text"
-                class="form-control mb-2 form-control form-control-lg"
-                id="search-input"
-                placeholder="Book Name"
-                name="voice-search"
-                autocomplete="off"
-                
-              />
-              <span id="voice-trigger">
-                <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 58 58" style="enable-background:new 0 0 58 58;" xml:space="preserve">
-                    <g>
-                        <path d="M44,28c-0.552,0-1,0.447-1,1v6c0,7.72-6.28,14-14,14s-14-6.28-14-14v-6c0-0.553-0.448-1-1-1s-1,0.447-1,1v6   c0,8.485,6.644,15.429,15,15.949V56h-5c-0.552,0-1,0.447-1,1s0.448,1,1,1h12c0.552,0,1-0.447,1-1s-0.448-1-1-1h-5v-5.051   c8.356-0.52,15-7.465,15-15.949v-6C45,28.447,44.552,28,44,28z"/>
-                        <path d="M29,46c6.065,0,11-4.935,11-11V11c0-6.065-4.935-11-11-11S18,4.935,18,11v24C18,41.065,22.935,46,29,46z M20,11   c0-4.963,4.038-9,9-9s9,4.037,9,9v24c0,4.963-4.038,9-9,9s-9-4.037-9-9V11z"/>
-                    </g>
-                </svg>
-            </span>
-        </fieldset>
-            </div>
+  </section>
+  <section>
+    <div class="container-fluid search">
+      <form id="search-form" method="post">
+        <div class="form-row align-items-center justify-content-center " style="padding-top: 5rem;">
+          <!--<div class="col-auto">
 
-            <div class="col-auto">
-              <button
-                type="submit"
-                class="btn btn-info mb-2 btn-lg"
-                style="width: 130px;"
-                value="submit"
-              >
-                Search
+            <div class="btn-group">
+              <button type="button " class="form-control btn btn-default btn-lg dropdown-toggle mb-2" data-toggle="dropdown">Search Type<span class="caret"></span>
               </button>
+              <ul class="dropdown-menu " role="menu">
+                <li>Title</li>
+                <li>Author</li>
+                <li>ISBN</li>
+                <li>Genre</li>
+              </ul>
             </div>
+
           </div>
-        </form>
-        
-        <?php
-            if(!empty($_POST)){
-            
-                $search = $_POST['voice-search'];
-                echo '<font style="color:red;">Result: </font>';
-                echo '<font style="color:blue;">You searched for: '.$search.'</font>';
+          drop-down menu for search type i.e. "Title", "Author"--> 
+          <div class="col-md-4">
+            <label class="sr-only " for="inlineFormInput">Name</label>
+            <input type="text" class="form-control mb-2 form-control form-control-lg" id="search-input" placeholder="Book Name" name="voice-search" autocomplete="on" />
+            <span id="voice-trigger">
+              <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 58 58" style="enable-background:new 0 0 58 58;" xml:space="preserve">
+                <g>
+                  <path d="M44,28c-0.552,0-1,0.447-1,1v6c0,7.72-6.28,14-14,14s-14-6.28-14-14v-6c0-0.553-0.448-1-1-1s-1,0.447-1,1v6   c0,8.485,6.644,15.429,15,15.949V56h-5c-0.552,0-1,0.447-1,1s0.448,1,1,1h12c0.552,0,1-0.447,1-1s-0.448-1-1-1h-5v-5.051   c8.356-0.52,15-7.465,15-15.949v-6C45,28.447,44.552,28,44,28z" />
+                  <path d="M29,46c6.065,0,11-4.935,11-11V11c0-6.065-4.935-11-11-11S18,4.935,18,11v24C18,41.065,22.935,46,29,46z M20,11   c0-4.963,4.038-9,9-9s9,4.037,9,9v24c0,4.963-4.038,9-9,9s-9-4.037-9-9V11z" />
+                </g>
+              </svg>
+            </span>
+            </fieldset>
+          </div>
 
-                // json -https://www.googleapis.com/books/v1/volumes?q=$search
-            }
-        ?>
-        <?php
+          <div class="col-auto">
+            <button type="submit" class="btn btn-info mb-2 btn-lg" style="width: 130px;" value="submit">
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
 
-            // API key, future ref
-            $API_KEY = '';
+      <?php
+      if (!empty($_POST)) {
 
-            // donot delete
-            require_once 'vendor/autoload.php';
+        $search = $_POST['voice-search'];
+        echo '<font style="color:red;">Result: </font>';
+        echo '<font style="color:blue;">You searched for: ' . $search . '</font>';
 
-            
-            $page = file_get_contents("https://www.googleapis.com/books/v1/volumes?q=".$search);
-            $data = json_decode($page, true);
-            echo "Title = " . $data['items'][0]['volumeInfo']['title'];
-            echo "<br/>Authors = " . @implode(",", $data['items'][0]['volumeInfo']['authors']); 
-        ?>
-        
-      </div>
-    </section>
+        // json -https://www.googleapis.com/books/v1/volumes?q=$search
+      }
+      ?>
+      <?php
 
-    <script
-      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-      crossorigin="anonymous"
-    ></script>
-  
-    <script
-      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-      crossorigin="anonymous"
-    ></script>
-    <script
-        src="http://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-        crossorigin="anonymous"></script>
-        <script src="main.js" ></script>
-        
-  </body>
+      // API key, future ref
+      $API_KEY = '';
+
+      // donot delete
+      require_once 'vendor/autoload.php';
+
+
+      $page = file_get_contents("https://www.googleapis.com/books/v1/volumes?q=" . urlencode($search)); //urlencode($search) converts Harry Potter to Harry+Potter
+      $data = json_decode($page, true);
+      echo "Title = " . $data['items'][0]['volumeInfo']['title'];
+      echo "<br/>Authors = " . @implode(",", $data['items'][0]['volumeInfo']['authors']);
+      ?>
+
+    </div>
+  </section>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="main.js"></script>
+
+</body>
+
 </html>
