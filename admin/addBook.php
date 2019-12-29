50b92a3656daf33a43821134169224fdb6e064a3
Manu1ND <manjunath2000@hotmail.com>
2019-12-29 21:45:04 +0530

    optimised search algo

--- admin/addBook.php
+++ admin/addBook.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,8 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
-  <script src="https://kit.fontawesome.com/97f3c2998d.js" crossorigin="anonymous"></script>
-  <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:700&display=swap" rel="stylesheet">
+
 
 
   <link rel="stylesheet" href="style.css" />
@@ -23,43 +21,39 @@
 <body>
 
 
-<script>
-  function addBook()
-  {
-    $.ajax({
-      url:'addQuery.php',
-      complete: function (response) {
-          //$('#output').html(response.responseText);
-          alert("reached add query ");
-      },
-      error: function () {
-          //$('#output').html('Bummer: there was an error!');
-          alert("did not reached add query ");
-      }
-  });
-  return false;
-  }
-</script>
-    <script>
-      function autoBookId(category) {
-        switch (category) {
-          case "maths":
-            document.getElementById('bookId').value = "MAT";
-            break;
-          case "science":
-            document.getElementById('bookId').value = "SCI";
-            break;
-          case "technology":
-            document.getElementById('bookId').value = "TECH";
-            break;
-          case "art":
-            document.getElementById('bookId').value = "ART";
-            break;
+  <script>
+    function addBook() {
+      $.ajax({
+        type: "POST",
+        url: 'addQuery.php',
+        data: {
+          title: 'Manu'
+        },
+        success: function(data) {
+          alert(data);
         }
+      });
+      return false;
+    }
+  </script>
+  <script>
+    function autoBookId(category) {
+      switch (category) {
+        case "maths":
+          document.getElementById('bookId').value = "MAT";
+          break;
+        case "science":
+          document.getElementById('bookId').value = "SCI";
+          break;
+        case "technology":
+          document.getElementById('bookId').value = "TECH";
+          break;
+        case "art":
+          document.getElementById('bookId').value = "ART";
+          break;
       }
-    </script>
-
-  </section>
+    }
+  </script>
   <section>
     <div class="container-fluid search" style="height:auto; color:powderblue; padding-bottom: 5rem; ">
       <form id="search-form" method="post">
@@ -68,7 +62,7 @@
             <label class="sr-only " for="inlineFormInput">Name</label>
             <input type="text" class="form-control mb-2 form-control form-control-lg" id="search-input" placeholder="Book Name" name="voice-search" autocomplete="on" />
             <span id="voice-trigger">
-              <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 58 58" style="enable-background:new 0 0 58 58;" xml:space="preserve">
+              <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="10px" y="10px" viewBox="0 0 58 58" style="enable-background:new 0 0 58 58;" xml:space="preserve">
                 <g>
                   <path d="M44,28c-0.552,0-1,0.447-1,1v6c0,7.72-6.28,14-14,14s-14-6.28-14-14v-6c0-0.553-0.448-1-1-1s-1,0.447-1,1v6   c0,8.485,6.644,15.429,15,15.949V56h-5c-0.552,0-1,0.447-1,1s0.448,1,1,1h12c0.552,0,1-0.447,1-1s-0.448-1-1-1h-5v-5.051   c8.356-0.52,15-7.465,15-15.949v-6C45,28.447,44.552,28,44,28z" />
                   <path d="M29,46c6.065,0,11-4.935,11-11V11c0-6.065-4.935-11-11-11S18,4.935,18,11v24C18,41.065,22.935,46,29,46z M20,11   c0-4.963,4.038-9,9-9s9,4.037,9,9v24c0,4.963-4.038,9-9,9s-9-4.037-9-9V11z" />
@@ -80,14 +74,11 @@
 
           <div class="col-auto">
             <button type="submit" class="btn btn-info mb-2 btn-lg" style="width: 130px;" value="submit">
-              Search 
+              Search
             </button>
           </div>
         </div>
       </form>
-      <script>
-        var i;
-      </script>
 
       <?php
       $search = '';
@@ -119,193 +110,231 @@
         $i = 0;
         foreach ($results as $item) {
           $title[$i] = $item['volumeInfo']['title'];
-          echo "Title: " . $title[$i];
-          echo "\t";
           $author[$i] = @implode(",", $item['volumeInfo']['authors']);
-          echo "Author: " . $author[$i];
-          echo "\t";
           $category[$i] = @implode(",", $item['volumeInfo']['categories']);
-          echo "Category: " . $category[$i];
-          echo "\t";
           $publisher[$i] = $item['volumeInfo']['publisher'];
-          echo "Publisher: " . $publisher[$i];
-          echo "\t";
           $publishedDate[$i] = $item['volumeInfo']['publishedDate'];
-          echo "Published Date: " . $publishedDate[$i] . "<br>";
           //volumeInfo.industryIdentifiers[].type
           $isbn[$i] = "";
           for ($n = 0; $n < count($item['volumeInfo']['industryIdentifiers']); $n++) {
             $isbn[$i] = $isbn[$i] . $item['volumeInfo']['industryIdentifiers'][$n]['identifier'] . " ";
           }
-          echo "ISBN: " . $isbn[$i] . "<br>";
           $description[$i] = $item['volumeInfo']['description'];
-          echo "Description: " . $description[$i] . "<br>";
           $pageCount[$i] = $item['volumeInfo']['pageCount'];
-          echo "Page Count: " . $pageCount[$i];
-          echo "\t";
           $country[$i] = $item['saleInfo']['country'];
-          echo "Country: " . $country[$i];
-          echo "\t";
           $currencyCode[$i] = $item['saleInfo']['listPrice']['currencyCode'];
           $amount[$i] = $item['saleInfo']['listPrice']['amount'];
           $money[$i] = $currencyCode[$i] . " " . $amount[$i];
 
           //Displays amount if available
-          if ($money[$i]) {
+          /*if ($money[$i]) {
             echo "Amount: " . $money[$i];
-          }
-
-        ?>
-          <?php
+          }*/
           $imgLink[$i] = $item['volumeInfo']['imageLinks']['thumbnail'];
-          ?>
-          <img src="<?= $imgLink[$i] ?>">
-
-          <!-- variables declared without var prefix mean that they are global
-          I removed var because of warnings-->
-          <script>
-            title = <?php echo json_encode($title); ?>
-          </script>
-          <script>
-            author = <?php echo json_encode($author); ?>
-          </script>
-          <script>
-            category = <?php echo json_encode($category); ?>
-          </script>
-          <script>
-            publisher = <?php echo json_encode($publisher); ?>
-          </script>
-          <script>
-            publishedDate = <?php echo json_encode($publishedDate); ?>
-          </script>
-          <script>
-            isbn = <?php echo json_encode($isbn); ?>
-          </script>
-          <script>
-            description = <?php echo json_encode($description); ?>
-          </script>
-          <script>
-            pageCount = <?php echo json_encode($pageCount); ?>
-          </script>
-          <script>
-            money = <?php echo json_encode($money); ?>
-          </script>
-          <script>
-            imgLink = <?php echo json_encode($imgLink); ?>
-          </script>
-
-          <button type="submit" class="btn btn-info btn-lg" id="<?= $i; ?>" onclick="autoFill(this.id)">Auto Fill</button>
-
-          <script>
-            function autoFill(i) {
-              // donot remove the comments in this method if the id isnt predefined in html form
-
-              document.getElementById('title').value = title[i];
-              document.getElementById('author').value = author[i];
-              document.getElementById('category').value = category[i];
-              document.getElementById('publisher').value = publisher[i];
-              document.getElementById('publishedDate').value = publishedDate[i];
-              document.getElementById('isbn').value = isbn[i];
-              document.getElementById('description').value = description[i];
-              document.getElementById('pageCount').value = pageCount[i];
-              document.getElementById('money').value = money[i];
-              if(imgLink[i])
-              {
-                document.getElementById('imgLink').src = imgLink[i];
-                document.getElementById('imgLink').hidden = false;
-              }
-            }
-          </script>
+        ?>
 
 
 
-      <?php
+          <table id="settings" class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <td><?= $title[$i] ?></td>
+              </tr>
+
+              <tr>
+                <th>Author</th>
+                <td><?= $author[$i] ?></td>
+
+              </tr>
+
+              <tr>
+                <th>Category</th>
+                <td><?= $category[$i] ?></td>
+              </tr>
+
+              <tr>
+                <th>Publisher</th>
+                <td><?= $publisher[$i] ?></td>
+              </tr>
+              <tr>
+                <th>Published Date</th>
+                <td><?= $publishedDate[$i] ?></td>
+              </tr>
+              <tr>
+                <th>ISBN</th>
+                <td><?= $isbn[$i] ?></td>
+              </tr>
+
+              <tr>
+                <th style="vertical-align: top;">Description</th>
+                <td>
+                  <div class="style-2" style="
+                                                          overflow-y: auto;
+                                                          min-height:20px;
+                            max-height: 200px;">
+                    <?= $description[$i] ?></div>
+                </td>
+              </tr>
+
+              <tr>
+                <th>Page Count</th>
+                <td><?= $pageCount[$i] ?></td>
+              </tr>
+              <tr>
+                <th>Country</th>
+                <td><?= $country[$i] ?></td>
+              </tr>
+              <tr>
+                <th>Amount</th>
+                <td><?= $money[$i] ?></td>
+              </tr>
+              <tr>
+                <th>Image</th>
+                <td><img src="<?= $imgLink[$i] ?>"></td>
+              </tr>
+
+            </thead>
+          </table>
+    <button type="submit" class="btn btn-info btn-lg" id="<?= $i; ?>" onclick="autoFill(this.id)">Auto Fill</button>
+<?php
           echo "<br>";
           echo "<br>";
           echo "<br>";
           $i++;
         }
       }
-      ?>
+?>
+</div>
 
-      
-    </div>
+<!-- variables declared without var prefix mean that they are global
+          I removed var because of warnings-->
+<script>
+  title = <?php echo json_encode($title); ?>
+</script>
+<script>
+  author = <?php echo json_encode($author); ?>
+</script>
+<script>
+  category = <?php echo json_encode($category); ?>
+</script>
+<script>
+  publisher = <?php echo json_encode($publisher); ?>
+</script>
+<script>
+  publishedDate = <?php echo json_encode($publishedDate); ?>
+</script>
+<script>
+  isbn = <?php echo json_encode($isbn); ?>
+</script>
+<script>
+  description = <?php echo json_encode($description); ?>
+</script>
+<script>
+  pageCount = <?php echo json_encode($pageCount); ?>
+</script>
+<script>
+  money = <?php echo json_encode($money); ?>
+</script>
+<script>
+  imgLink = <?php echo json_encode($imgLink); ?>
+</script>
+<script>
+  function autoFill(i) {
+    // donot remove the comments in this method if the id isnt predefined in html form
+
+    document.getElementById('title').value = title[i];
+    document.getElementById('author').value = author[i];
+    document.getElementById('category').value = category[i];
+    document.getElementById('publisher').value = publisher[i];
+    document.getElementById('publishedDate').value = publishedDate[i];
+    document.getElementById('isbn').value = isbn[i];
+    document.getElementById('description').value = description[i];
+    document.getElementById('pageCount').value = pageCount[i];
+    document.getElementById('money').value = money[i];
+    if (imgLink[i]) {
+      document.getElementById('imgLink').src = imgLink[i];
+      document.getElementById('imgLink').hidden = false;
+    }
+  }
+</script>
+</div>
   </section>
 
 
 
-  <section>
+  <form id="myform">
     <div class="container-fluid form " style="height:auto; ">
       <h1 style=" color:#46b5d1;">ADD BOOK</h1>
       <form>
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Title</label>
           <div class="col-md-6">
-            <input type="text" class="form-control" id="title" placeholder="Example input">
+            <input type="text" class="form-control" name="title" id="title" placeholder="Example input">
           </div>
         </div>
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Author</label>
           <div class="col-sm-6">
-            <input type="text" class="form-control" id="author" placeholder="Example input">
+            <input type="text" class="form-control" name="author" id="author" placeholder="Example input">
           </div>
         </div>
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Category</label>
           <div class="col-sm-6">
-            <input type="text" class="form-control" id="category" placeholder="Example input">
+            <input type="text" class="form-control" name="category" id="category" placeholder="Example input">
           </div>
         </div>
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">ISBN</label>
           <div class="col-sm-6">
-            <input type="number" class="form-control" id="isbn" placeholder="Example input">
+            <input type="text" class="form-control" name="isbn" id="isbn" placeholder="Example input">
           </div>
         </div>
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Description</label>
           <div class="col-sm-6">
-            <textarea class="form-control" id="description" rows="3"></textarea>
+            <textarea class="form-control" name="description" id="description" rows="3"></textarea>
           </div>
         </div>
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Publisher</label>
           <div class="col-sm-6">
-            <input type="text" class="form-control" id="publisher" placeholder="Example input">
+            <input type="text" class="form-control" name="publisher" id="publisher" placeholder="Example input">
           </div>
         </div>
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Published Date</label>
           <div class="col-sm-6">
-            <input type="text" class="form-control" id="publishedDate" placeholder="Example input">
+            <input type="text" class="form-control" name="publishedDate" id="publishedDate" placeholder="Example input">
           </div>
         </div>
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Page count</label>
           <div class="col-sm-6">
-            <input type="text" class="form-control" id="pageCount" placeholder="Example input">
+            <input type="text" class="form-control" name="pageCount" id="pageCount" placeholder="Example input">
           </div>
         </div>
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Price</label>
           <div class="col-sm-6">
-            <input type="text" class="form-control" id="money" placeholder="Example input">
+            <input type="text" class="form-control" name="money" id="money" placeholder="Example input">
           </div>
         </div>
         <div class="form-group form-row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Image</label>
-          <img id="imgLink" hidden="true" src="" alt="your image" width="100" height="100" />
+          <img name="imgLink" id="imgLink" hidden="true" src="" alt="your image" width="100" height="100" />
           <input id="imgFile" type="file" onchange="document.getElementById('imgLink').src = window.URL.createObjectURL(this.files[0]), document.getElementById('imgLink').hidden= false">
         </div>
         <div class="form-group form-row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Quantity</label>
           <div class="col-sm-6">
-            <input type="number" class="form-control" id="quantity" placeholder="Example input">
+            <input type="number" class="form-control" name="quantity" id="quantity" placeholder="Example input">
           </div>
         </div>
         <div class="form-group form-row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Category1</label>
-          <select class="col-sm-2 col-form-label" id="category1" onclick="autoBookId(this.value)">
+          <select class="col-sm-2 col-form-label" name="category1" id="category1" onclick="autoBookId(this.value)">
             <option value="maths">Maths</option>
             <option value="science">Science</option>
             <option value="technology">Technology</option>
@@ -316,17 +345,22 @@
         <div class="form-group row align-items-center justify-content-center ">
           <label for="" class="col-sm-2 col-form-label">Book ID</label>
           <div class="col-sm-6">
-            <input type="text" class="form-control" id="bookId" placeholder="Example input" value="MAT">
+            <input type="text" class="form-control" name="bookId" id="bookId" placeholder="Example input" value="MAT">
           </div>
         </div>
         <div class="form-group form-row align-items-center justify-content-center ">
           <div class="col-sm-1 addbook">
-            <button type="submit" class="btn btn-info btn-lg" onclick=addBook()>Add Book</button>
+            <button type="submit" class="btn btn-info btn-lg" onclick="addBook()">Add Book</button>
           </div>
         </div>
       </form>
     </div>
+  </form>
 
+  <script src="https://kit.fontawesome.com/97f3c2998d.js" crossorigin="anonymous"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:700&display=swap" rel="stylesheet">
+  <script src="main.js"></script>
 
 </body>
 
