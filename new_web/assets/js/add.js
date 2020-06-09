19959cfd2412940b6652c4a12ad6d8c6021958c5
Amit Ramani <amit97ramani@gmail.com>
2020-06-09 19:59:59 +0530

    add books header section done

---
+++ new_web/assets/js/add.js
@@ -0,0 +1,61 @@
+const searchForm = document.querySelector(".search-form");
+const searchFormInput = searchForm.querySelector("input"); // <=> document.querySelector("#search-form input");
+
+// The speech recognition interface lives on the browser’s window object
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition; // if none exists -> undefined
+
+if (SpeechRecognition) {
+  console.log("Your Browser supports speech Recognition");
+
+  const recognition = new SpeechRecognition();
+  // recognition.lang = "en-US";
+
+  searchForm.insertAdjacentHTML(
+    "beforeend",
+    '<button type="button" class="mic-button"><i i class= "fa fa-microphone " aria-hidden="true" ></i></button >'
+  );
+
+  const micBtn = searchForm.querySelector(".mic-button");
+  const micIcon = micBtn.firstElementChild;
+
+  micBtn.addEventListener("click", micBtnClick);
+  function micBtnClick() {
+    if (micIcon.classList.contains("fa-microphone")) {
+      // Start Voice Recognition
+      recognition.start(); // First time you have to allow access to mic!
+    } else {
+      recognition.stop();
+    }
+  }
+
+  recognition.addEventListener("start", startSpeechRecognition); // <=> recognition.onstart = function() {...}
+  function startSpeechRecognition() {
+    micIcon.classList.remove("fa-microphone");
+    micIcon.classList.add("fa-microphone-slash");
+    searchFormInput.focus();
+    console.log("Voice activated, SPEAK");
+  }
+
+  recognition.addEventListener("end", endSpeechRecognition); // <=> recognition.onend = function() {...}
+  function endSpeechRecognition() {
+    micIcon.classList.remove("fa-microphone-slash");
+    micIcon.classList.add("fa-microphone");
+    searchFormInput.focus();
+    console.log("Speech recognition service disconnected");
+  }
+
+  recognition.addEventListener("result", resultOfSpeechRecognition); // <=> recognition.onresult = function(event) {...} - Fires when you stop talking
+  function resultOfSpeechRecognition(event) {
+    const transcript = event.results[0][0].transcript;
+    console.log(transcript);
+    searchFormInput.value = transcript;
+    searchFormInput.focus();
+    // setTimeout(() => {
+    //   searchForm.submit();
+    // }, 500);
+  }
+} else {
+  console.log("Your Browser does not support speech Recognition");
+  info.textContent = "Your Browser does not support Speech Recognition";
+}
