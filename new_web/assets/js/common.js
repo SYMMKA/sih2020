e81c36d63d7deedd64c7c7b168d97bdfac606228
Amit Ramani <amit97ramani@gmail.com>
2020-06-11 21:32:01 +0530

    ...

--- new_web/assets/js/common.js
+++ new_web/assets/js/common.js
@@ -6,13 +6,9 @@ window.onscroll = function () {
 function scrollFunction() {
   x = document.getElementsByClassName("navbar")[0];
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-    x.classList.remove("bg-transparent");
-    x.classList.add("bg-white");
     x.classList.add("floatingNav");
     x.style.backgroundColor = "white";
   } else {
-    x.classList.remove("bg-white");
-    x.classList.add("bg-transparent");
     x.classList.remove("floatingNav");
     x.style.backgroundColor = "transparent";
   }
