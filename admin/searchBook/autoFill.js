2e65d26b9d7277ada23a4d21c66476547a3e118c
Amit Ramani <amit97ramani@gmail.com>
2020-06-23 20:44:43 +0530

    report page done

--- admin/searchBook/autoFill.js
+++ admin/searchBook/autoFill.js
@@ -1,21 +1,21 @@
 function autoFillBook(i) {
-	document.getElementById("displayBookCopies").innerHTML = "";
-	var formData = new FormData();
-	formData.append("bookID", bookID[i]);
-	$.ajax({
-		type: "POST",
-		url: "searchBook/copies.php",
-		data: formData,
-		contentType: false, // Dont delete this (jQuery 1.6+)
-		processData: false, // Dont delete this
-		success: function (data) {
-			var html = "";
-			html +=
-				`<div class="modal-content">
+  document.getElementById("displayBookCopies").innerHTML = "";
+  var formData = new FormData();
+  formData.append("bookID", bookID[i]);
+  $.ajax({
+    type: "POST",
+    url: "searchBook/copies.php",
+    data: formData,
+    contentType: false, // Dont delete this (jQuery 1.6+)
+    processData: false, // Dont delete this
+    success: function (data) {
+      var html = "";
+      html +=
+        `<div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">` +
-				title[i] +
-				`</h5>
+        title[i] +
+        `</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -23,25 +23,25 @@ function autoFillBook(i) {
             <div class="modal-body">
                 <div class="row">
                     <div class="col-12">
-                    <div class="row row-cols-1 row-cols-md-4" style="height:500px; overflow-y: scroll;">
+                    <div class="row row-cols-1 row-cols-md-4" style="height:65vh; overflow-y: scroll;">
                     <input type="hidden" id="elementID">
                     <input type="hidden" id="reservedBy">
                     <input type="hidden" id="copyBookID" value="` +
-				bookID[i] +
-				`">
+        bookID[i] +
+        `">
                     <input type="hidden" id="copyID">`;
 
-			if (data) {
-				var data = JSON.parse(data);
-				data.forEach(function (item, index) {
-					html += `
+      if (data) {
+        var data = JSON.parse(data);
+        data.forEach(function (item, index) {
+          html += `
                 <div class="col mb-4">
-                        <div class="card h-100">`;
-					html +=
-						` <div class="card-body" style="padding: 1rem;">
+                        <div class="card">`;
+          html +=
+            ` <div class="card-body" style="padding: 1rem;">
                             <h5 class="card-title text-center"><strong>Copy No: </strong>` +
-						item.copyno +
-						`</h4>
+            item.copyno +
+            `</h4>
                             <p class="card-text">                   
                         <div class="row no-gutters">
                                 <div Class="col-4">
@@ -49,21 +49,21 @@ function autoFillBook(i) {
                                 </div>
                                 <div Class="col-8">
                             ` +
-						item.oldID +
-						`
+            item.oldID +
+            `
                                 </div>
                             </div>`;
-					var reservedBy = "";
-					if (item.status == "reserved" && item.returnTime > item.currentTime) {
-						html +=
-							`<div class="row no-gutters">
+          var reservedBy = "";
+          if (item.status == "reserved" && item.returnTime > item.currentTime) {
+            html +=
+              `<div class="row no-gutters">
                                 <div Class="col-4">
                                 <strong>Reserved by:</strong>
                                 </div>
                                 <div Class="col-8">
                             ` +
-							item.stud_ID +
-							`
+              item.stud_ID +
+              `
                                 </div></br>
                             </div>
                             <div class="row no-gutters">
@@ -72,21 +72,21 @@ function autoFillBook(i) {
                                 </div>
                                 <div Class="col-8">
                             ` +
-							item.time +
-							`
+              item.time +
+              `
                                 </div></br>
                             </div>`;
-						reservedBy = item.stud_ID;
-					} else if (item.status == "issued") {
-						html +=
-							`<div class="row no-gutters">
+            reservedBy = item.stud_ID;
+          } else if (item.status == "issued") {
+            html +=
+              `<div class="row no-gutters">
                                 <div Class="col-4">
                                     <strong>Issued by:</strong>
                                 </div>
                                 <div Class="col-8">
                             ` +
-							item.stud_ID +
-							`
+              item.stud_ID +
+              `
                                 </div></br>
                             </div>
                             <div class="row no-gutters">
@@ -95,47 +95,47 @@ function autoFillBook(i) {
                                 </div>
                                 <div Class="col-8">
                             ` +
-							item.time +
-							`
+              item.time +
+              `
                                 </div></br>
                             </div>`;
-					} else {
-						html += `</br>
+          } else {
+            html += `</br>
                             <div class="text-center">
                                 <strong>Available</strong></br>
                             </div>`;
-					}
-					html += `   </p>
+          }
+          html += `   </p>
                             </div>
                             <div class="card-footer bg-white">
                             <div class="row text-center">
                             <div class="col-12">`;
-					if (item.status == "issued") {
-						html +=
-							`<button type="submit" form="returnBookForm" class="btn btn-info btn-block btn-sm" name="issueReturnCopy" onclick="autoFillReturnBook('` +
-							item.copyID +
-							`','` +
-							i +
-							`')">Return Copy`;
-					} else {
-						html +=
-							`<button type="button" class="btn btn-info btn-block btn-sm" name="issueBookCopy" id="issueBookCopy" onclick="autoFillIssueBook('` +
-							item.copyID +
-							`','` +
-							item.oldID +
-							`','` +
-							reservedBy +
-							`','` +
-							i +
-							`')">Issue Copy`;
-					}
-					html +=
-						`</button>
+          if (item.status == "issued") {
+            html +=
+              `<button type="submit" form="returnBookForm" class="btn btn-info btn-block btn-sm" name="issueReturnCopy" onclick="autoFillReturnBook('` +
+              item.copyID +
+              `','` +
+              i +
+              `')">Return Copy`;
+          } else {
+            html +=
+              `<button type="button" class="btn btn-info btn-block btn-sm" name="issueBookCopy" id="issueBookCopy" onclick="autoFillIssueBook('` +
+              item.copyID +
+              `','` +
+              item.oldID +
+              `','` +
+              reservedBy +
+              `','` +
+              i +
+              `')">Issue Copy`;
+          }
+          html +=
+            `</button>
                     <button type="submit" form="deleteCopyForm" class="btn btn-info btn-block btn-sm" name="deleteCopyCopy" onclick="autoFillDeleteCopy('` +
-						item.copyID +
-						`','` +
-						i +
-						`')">
+            item.copyID +
+            `','` +
+            i +
+            `')">
                         Delete Copy
                     </button>
                           </div>
@@ -143,9 +143,9 @@ function autoFillBook(i) {
                     </div>
                 </div>
             </div>`;
-				});
-			}
-			html += `
+        });
+      }
+      html += `
        </div>
         </div>
                 </div>
@@ -156,8 +156,8 @@ function autoFillBook(i) {
 					<button type="button" class="btn btn-primary">Understood</button>
 				</div>
 			</div>`;
-			document.getElementById("displayBookCopies").innerHTML = html;
-		},
-		//Other options
-	});
+      document.getElementById("displayBookCopies").innerHTML = html;
+    },
+    //Other options
+  });
 }
--- admin/shelf/addCopy/addCopyFill.js
+++ admin/shelf/addCopy/addCopyFill.js
@@ -1,38 +1,38 @@
 function addCopyFill(i) {
-	var formData = new FormData();
-	formData.append("bookID", bookID[i]);
-	$.ajax({
-		type: "POST",
-		url: "addCopy/showAddCopies.php",
-		data: formData,
-		contentType: false, // Dont delete this (jQuery 1.6+)
-		processData: false, // Dont delete this
-		success: function (data) {
-			var html = "";
-			html +=
-				`<div class="modal-content">
+  var formData = new FormData();
+  formData.append("bookID", bookID[i]);
+  $.ajax({
+    type: "POST",
+    url: "addCopy/showAddCopies.php",
+    data: formData,
+    contentType: false, // Dont delete this (jQuery 1.6+)
+    processData: false, // Dont delete this
+    success: function (data) {
+      var html = "";
+      html +=
+        `<div class="modal-content">
                 <div class="modal-header">
                   <h5 class="modal-title" id="staticBackdropLabel">` +
-				title[i] +
-				`</h5>
+        title[i] +
+        `</h5>
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
                 </div>
                 <div class="modal-body">
-                  <div class="row row-cols-1 row-cols-md-3" id='displayBookCopies' style="height: 500px; overflow-y: scroll;">`;
-			if (data) {
-				var data = JSON.parse(data);
-				data.forEach(function (item, index) {
-					html +=
-						`
+                  <div class="row row-cols-1 row-cols-md-4" id='displayBookCopies' style="height: 65vh; overflow-y: scroll;">`;
+      if (data) {
+        var data = JSON.parse(data);
+        data.forEach(function (item, index) {
+          html +=
+            `
             
               <div class="col mb-4">
-                <div class="card h-100">
+                <div class="card">
                   <div class="card-body" style="padding: 1rem;">
                     <h4 class="card-title text-center"><strong>Copy No: </strong>` +
-						item.copyno +
-						`</h4>
+            item.copyno +
+            `</h4>
                     <p class="card-text">                   
                    <div class="row no-gutters">
                         <div Class="col-4">
@@ -40,8 +40,8 @@ function addCopyFill(i) {
                         </div>
                         <div Class="col-8">
                         ` +
-						item.copyID +
-						`
+            item.copyID +
+            `
                         </div></br>
                     </div>
 
@@ -51,8 +51,8 @@ function addCopyFill(i) {
                         </div>
                         <div Class="col-8">
                         ` +
-						isbn[i] +
-						`
+            isbn[i] +
+            `
                         </div></br>
                     </div>
 
@@ -62,8 +62,8 @@ function addCopyFill(i) {
                         </div>
                         <div Class="col-8">
                         ` +
-						item.oldID +
-						`
+            item.oldID +
+            `
                         </div></br>
                     </div>
 
@@ -73,41 +73,41 @@ function addCopyFill(i) {
                         </div>
                         <div Class="col-8">
                         ` +
-						item.shelfID +
-						`
+            item.shelfID +
+            `
                         </div></br>
                     </div></p></div>
                          <div class="card-footer bg-white">
           <div class="row text-center">
                           <div class="col-12">`;
-					if (item.shelfID == shelfID)
-						html +=
-							`
+          if (item.shelfID == shelfID)
+            html +=
+              `
                             <button type="submit" class="btn btn-info" onclick="removeCopy('` +
-							item.copyID +
-							`','` +
-							i +
-							`')">
+              item.copyID +
+              `','` +
+              i +
+              `')">
                               Remove`;
-					else {
-						html +=
-							`             <button type="submit" class="btn btn-info" onclick="addCopy('` +
-							item.copyID +
-							`','` +
-							i +
-							`')">`;
-						if (item.shelfID == null) html += `Add to Shelf`;
-						else html += `Change Shelf`;
-					}
-					html += `</button>
+          else {
+            html +=
+              `             <button type="submit" class="btn btn-info" onclick="addCopy('` +
+              item.copyID +
+              `','` +
+              i +
+              `')">`;
+            if (item.shelfID == null) html += `Add to Shelf`;
+            else html += `Change Shelf`;
+          }
+          html += `</button>
                         </div>
                           </div>
                         </div>
                       </div>
                     </div>`;
-				});
+        });
 
-				html += `
+        html += `
                    
                   </div>
                 </div>
@@ -116,43 +116,43 @@ function addCopyFill(i) {
                     <button type="button" class="btn btn-primary">Understood</button>
                 </div>
             </div>`;
-				document.getElementById("displayBookCopies").innerHTML = html;
-			}
-		},
-		//Other options
-	});
+        document.getElementById("displayBookCopies").innerHTML = html;
+      }
+    },
+    //Other options
+  });
 }
 
 function addCopy(copyID, i) {
-	var formData = new FormData();
-	formData.append("copyID", copyID);
-	formData.append("shelfID", shelfID);
-	$.ajax({
-		type: "POST",
-		url: "addCopy/addCopyShelf.php",
-		data: formData,
-		contentType: false, // Dont delete this (jQuery 1.6+)
-		processData: false, // Dont delete this
-		success: function (data) {
-			console.log(data);
-			addCopyFill(i);
-		},
-	});
+  var formData = new FormData();
+  formData.append("copyID", copyID);
+  formData.append("shelfID", shelfID);
+  $.ajax({
+    type: "POST",
+    url: "addCopy/addCopyShelf.php",
+    data: formData,
+    contentType: false, // Dont delete this (jQuery 1.6+)
+    processData: false, // Dont delete this
+    success: function (data) {
+      console.log(data);
+      addCopyFill(i);
+    },
+  });
 }
 
 function removeCopy(copyID, i) {
-	var formData = new FormData();
-	formData.append("copyID", copyID);
-	$.ajax({
-		type: "POST",
-		url: "removeCopy.php",
-		data: formData,
-		contentType: false, // Dont delete this (jQuery 1.6+)
-		processData: false, // Dont delete this
-		success: function (data) {
-			console.log(data);
-			addCopyFill(i);
-		},
-		//Other options
-	});
+  var formData = new FormData();
+  formData.append("copyID", copyID);
+  $.ajax({
+    type: "POST",
+    url: "removeCopy.php",
+    data: formData,
+    contentType: false, // Dont delete this (jQuery 1.6+)
+    processData: false, // Dont delete this
+    success: function (data) {
+      console.log(data);
+      addCopyFill(i);
+    },
+    //Other options
+  });
 }
