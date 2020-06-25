c5264950bac91e7cebcf7bd31a75a49348b13ba0
Amit Ramani <amit97ramani@gmail.com>
2020-06-25 23:50:13 +0530

    voice navigation beta v1 in add.html

--- admin/assets/js/voice-search.js
+++ admin/assets/js/voice-search.js
@@ -10,6 +10,8 @@ if (SpeechRecognition) {
   console.log("Your Browser supports speech Recognition");
 
   const recognition = new SpeechRecognition();
+  const recognition2 = new SpeechRecognition();
+  recognition2.continuous = true;
   // recognition.lang = "en-US";
 
   searchFormInput.insertAdjacentHTML(
@@ -19,6 +21,8 @@ if (SpeechRecognition) {
 
   const micBtn = searchForm.querySelector(".mic-button");
   const micIcon = micBtn.firstElementChild;
+  const voiceBtn = document.querySelector(".voice-button");
+  const voiceIcon = voiceBtn.firstElementChild;
 
   micBtn.addEventListener("click", micBtnClick);
   function micBtnClick() {
@@ -27,6 +31,16 @@ if (SpeechRecognition) {
       recognition.start(); // First time you have to allow access to mic!
     } else {
       recognition.stop();
+    }
+  }
+
+  voiceBtn.addEventListener("click", voiceBtnClick);
+  function voiceBtnClick() {
+    if (voiceIcon.classList.contains("fa-microphone")) {
+      // Start Voice Recognition
+      recognition2.start();
+    } else {
+      recognition2.stop();
     }
   }
 
@@ -52,6 +66,38 @@ if (SpeechRecognition) {
     console.log(transcript);
     input.value = transcript;
     input.focus();
+    // setTimeout(() => {
+    //   searchForm.submit();
+    // }, 500);
+  }
+
+  recognition2.addEventListener("start", startSpeechRecognition2); // <=> recognition.onstart = function() {...}
+  function startSpeechRecognition2() {
+    voiceIcon.classList.remove("fa-microphone");
+    voiceIcon.classList.add("fa-microphone-slash");
+    console.log("Voice activated, SPEAK");
+  }
+
+  recognition2.addEventListener("end", endSpeechRecognition2); // <=> recognition.onend = function() {...}
+  function endSpeechRecognition2() {
+    voiceIcon.classList.remove("fa-microphone-slash");
+    voiceIcon.classList.add("fa-microphone");
+    console.log("Speech recognition service disconnected");
+  }
+
+  recognition2.addEventListener("result", resultOfSpeechRecognition2); // <=> recognition.onresult = function(event) {...} - Fires when you stop talking
+  function resultOfSpeechRecognition2(event) {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    console.log(transcript.toLowerCase());
+    if (transcript.toLowerCase().trim() === "stop recording") {
+      endSpeechRecognition2();
+    } else if (!searchFormInput.value) {
+      searchFormInput.value = transcript;
+    } else transcript.toLowerCase().trim() == "home";
+    if (transcript.toLowerCase().trim() === "go") {
+      window.location.href = "http://www.w3schools.com";
+    }
     // setTimeout(() => {
     //   searchForm.submit();
     // }, 500);
