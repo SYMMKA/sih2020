7fcc538d2b5a0f4181bc56222beddd668d05f127
Kaushik70 <54993614+Kaushik70@users.noreply.github.com>
2020-06-25 14:33:13 +0530

    Add files via upload

---
+++ DDC Generation/Form.php
@@ -0,0 +1,38 @@
+<html>
+	<script src="https://code.jquery.com/jquery-latest.js"></script>
+		<script>
+		function get_ddc()
+		{
+			var ip=$("#isbn").val();
+			var title=$("#title").val();
+			var author=$("#author").val();
+			alert(ip);
+			alert(title);
+			$.get("form_get.php",{ip:ip,title:title,author:author},
+			function(data)
+			{
+				$("#json_response").html(data);
+			}
+
+			);
+			
+		}
+	</script>
+	<body>
+		<center>
+			<h3>Write ISBN number </h3>
+			<form>
+				ISBN number *not needed*: <input name="isbn" id="isbn" type="text" /> <br />  <!-- Not Compusary -->
+				Title *compulsary*: <input name="title" id="title" type="text" /> <br />  <!-- Compusary -->
+				Author*compulsary*: <input name="author" id="author" type="text" /> <br />  <!-- Compusary -->
+				<input type="button" value="Submit" onclick="get_ddc()"/>
+			</form>
+			<br>----------------------------------------------
+			<div id="json_response"></div>
+		</center>
+	</body>
+</html>
+
+
+
+
---
+++ DDC Generation/Logic code.php
@@ -0,0 +1,56 @@
+<html>
+  <?php
+  $ip=$_GET['ip'];
+  $title=$_GET['title'];
+  $author=$_GET['author'];
+  $title = rawurlencode($title);
+  $author = rawurlencode($author);
+  $url = "http://classify.oclc.org/classify2/Classify?author=$author&title=$title&summary=true";
+  $response = simplexml_load_file($url);
+  $code = $response->response['code'];
+  echo $code;
+  echo "     ";
+  if ($code == 0 || $code == 2 || $code == 4)
+    {
+      $haa = $response->works->work[1]['owi'];
+      echo "Owi1: ";
+      echo ($haa);
+      echo "\n";
+      if(isset($response->recommendations->ddc))
+      {
+        echo $response->recommendations->ddc->mostPopular['sfa'];
+      }
+      else
+      {
+        $url = "http://classify.oclc.org/classify2/Classify?owi=$haa";
+        $response = simplexml_load_file($url);
+        if(isset($response->recommendations->ddc))
+        {
+          echo $response->recommendations->ddc->mostPopular['sfa'];
+        }
+        else
+        {
+          $haa = $response->works->work[2]['owi'];
+          echo "Owi2: ";
+          echo ($haa);
+          echo "\n";
+          $url = "http://classify.oclc.org/classify2/Classify?owi=$haa";
+          $response = simplexml_load_file($url);
+          if(isset($response->recommendations->ddc))
+          {
+            echo "DDC: ";
+            echo $response->recommendations->ddc->mostPopular['sfa'];
+          }
+          else
+          {
+            echo "NO DDC";
+          }
+        }
+      }
+    }
+  else{
+    echo "No DDC";
+  }
+  #if (!isset($reddc->mostPopular))
+  ?>
+</html>
