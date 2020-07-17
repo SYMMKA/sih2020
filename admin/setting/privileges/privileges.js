f91a06fca6b639736ecf28d73c119527f6cc07a0
Amit Ramani <amit97ramani@gmail.com>
2020-07-17 22:02:05 +0530

    privileges done

---
+++ admin/setting/privileges/privileges.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $("#SavePrivileges").on("click", function () {
+        var issueAccess = $("input[name='issueAccess']:checked").val();
+        var returnAccess = $("input[name='returnAccess']:checked").val();
+        var addBookAccess = $("input[name='addBookAccess']:checked").val();
+        var updateBookAccess = $(
+            "input[name='updateBookAccess']:checked"
+        ).val();
+        var shelfModifyAccess = $(
+            "input[name='shelfModifyAccess']:checked"
+        ).val();
+        var bookShelfAccess = $("input[name='bookShelfAccess']:checked").val();
+        var semBranchModifyAccess = $(
+            "input[name='semBranchModifyAccess']:checked"
+        ).val();
+        var bookSemBranchAccess = $(
+            "input[name='bookSemBranchAccess']:checked"
+        ).val();
+        var settingsAccess = $("input[name='settingsAccess']:checked").val();
+        $.ajax({
+            url: "setting/privileges/privileges.php",
+            method: "POST",
+            dataType: "text",
+            data: {
+                issueAccess: issueAccess,
+                returnAccess: returnAccess,
+                addBookAccess: addBookAccess,
+                updateBookAccess: updateBookAccess,
+                shelfModifyAccess: shelfModifyAccess,
+                bookShelfAccess: bookShelfAccess,
+                semBranchModifyAccess: semBranchModifyAccess,
+                bookSemBranchAccess: bookSemBranchAccess,
+                settingsAccess: settingsAccess,
+            },
+            success: function (data) {
+                alert(data);
+            },
+            error: function (error) {
+                alert(error);
+            },
+        });
+    });
+});
